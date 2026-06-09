3581ef5764c302b96571a87f9a31577df68c1c96
DoarBell <vidalkijuro@gmail.com>
2026-06-09 12:52:03 -0600

    js is love js is life

--- P_INDEX/counter.js
+++ P_INDEX/counter.js
@@ -10,18 +10,22 @@ function count(command) {
             //pass a varible into a string
             alert(`Count is now ${counter}`)
         }
-        
-        //reset button
-        if (command === 'reset'){
-            counter = 0
-            document.querySelector('h1').innerHTML = counter;
-            alert(`Count restarted to ${counter}`)
-        }
     }
     //add event listener helps you make a function without name and then run it when in this case the content of the 
     //whole page is done loading 'DOMContentLoaded'
     //you can also just pass the name of the function like in the second line you can addEventListener pasing ('trigger', and the funtion's name)
     document.addEventListener('DOMContentLoaded', function() {
                                         //.addEventListener('onclick', count)
-        document.querySelector('button').onclick = count;
+        document.querySelector('#count').onclick = function() {
+            counter++;
+            document.querySelector('#number').innerHTML = counter;
+            if(counter % 10 === 0){
+                alert(`Count in now ${counter}`)
+            }
+        };
+        //Reset button
+        document.querySelector('#reset').onclick = function() {
+            counter = 0; document.querySelector('#number').innerHTML = counter;
+            alert(`Counter restarted to ${counter}`)
+        }
     });
