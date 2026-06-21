5bab5025643a6064d0d30f8526ee710d696875c8
DoarBell <vidalkijuro@gmail.com>
2026-06-20 20:55:14 -0600

    local storage usage

--- P_INDEX/counter.js
+++ P_INDEX/counter.js
@@ -1,17 +1,20 @@
 //initiate a new variable
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+};
 //This is not being called in the program, it is not needed, it is just and example for you baby ;)
 //make a new function
-function count(command) {
+/* function count(command) {
         //count button
+        let counter = localStorage.getItem('counter');
         counter++;
         document.querySelector('h1').innerHTML = counter;
-        
-        /* if (counter % 10 === 0){
+        localStorage.setItem('counter', counter)
+        if (counter % 10 === 0){
             //pass a varible into a string
             alert(`Count is now ${counter}`);
-        } */
-    }
+        }
+    } */
 //this is the input normalizer for when the user wants to use the keyboard to type in the calculator
 function calc(input){
     input.value = input.value.replace(/[^().%x+-/0-9]/g, '');
@@ -20,22 +23,24 @@ function calc(input){
     //whole page is done loading 'DOMContentLoaded'
     //you can also just pass the name of the function like in the second line you can addEventListener pasing ('trigger', and the funtion's name)
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#number').innerHTML = localStorage.getItem('counter');
                                     //.addEventListener('onclick', count)
     document.querySelector('#count').onclick = function() {
+        let counter = localStorage.getItem('counter');
         counter++;
         document.querySelector('#number').innerHTML = counter;
+        localStorage.setItem('counter', counter)
         /* if(counter % 10 === 0){
             alert(`Count in now ${counter}`)
         } */
     };
     //Reset button
     document.querySelector('#reset').onclick = function() {
-        counter = 0;
+        localStorage.setItem('counter', 0)
+        let counter = localStorage.getItem('counter')
         document.querySelector('#number').innerHTML = counter;
         alert(`Counter restarted to ${counter}`);
-    }
-
-    document.querySelector('#count').onclick = () => { setInterval(count, 1000) };
+    };
 
     //Calculator
     const formulaInput = document.querySelector('#formula');
