0b50584408afe41ac1f780c7d6abb148b4e1079d
DoarBell <vidalkijuro@gmail.com>
2026-06-20 20:21:30 -0600

    bug fixed

--- P_INDEX/tasks.js
+++ P_INDEX/tasks.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // By default the submit button is disabled
+    subbutton = document.querySelector('#submit');
+    subbutton.disabled = true;
+    document.querySelector('#task').onkeyup = () => {
+        if (document.querySelector('#task').value === '') {
+            subbutton.disabled = true;
+        }else{
+        subbutton.disabled = false;
+        }
+    }
     document.querySelector('#go').onsubmit = function() {
-        const task = document.querySelector('#task').value;
+        let task = document.querySelector('#task').value;
         //console.log(task); works as a print in the logs part of the page's console
         
         const li = document.createElement('li');
         li.innerHTML = task;
 
         document.querySelector('#tasks').append(li);
+
+        subbutton.disabled = true
+        document.querySelector('#task').value = "";
 
         //stop form from submiting
         return false;
